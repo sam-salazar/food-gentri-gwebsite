446c348bcdbf4c314fb2388bc75b886fdde1c8c7
Vi Vu <83096281+aletterofthealphabet@users.noreply.github.com>
2025-07-23 14:57:18 -0400

    Added rough nav bar with jump links

--- main.js
+++ main.js
@@ -1,2 +1,2 @@
-// add buttons that take the user to different sections
-// or hide and show said sections
+// add buttons that take the user to different sections << 7/23 implemented via HTML, but needs JS to work on mobile
+// or hide and show said sections << how do we want to implement this? ie mimicing different pages, making different pages or a toggle on one page?
