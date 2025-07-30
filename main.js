2fa3b58fea77b7784dbbba4cb2ef44e3ea35d378
Kevin Valencia <109239342+kvalenc@users.noreply.github.com>
2025-07-30 11:00:26 -0700

    sync

--- main.js
+++ main.js
@@ -1,2 +1,6 @@
 // add buttons that take the user to different sections
 // or hide and show said sections
+
+document.addEventListener("DOMContentLoaded", function () {
+    alert("Welcome to our Food Gentrification page!");
+});
