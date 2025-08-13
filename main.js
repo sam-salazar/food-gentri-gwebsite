0c79375a77d29577b0c2da128ba00059170138cc
Kevin Valencia <109239342+kvalenc@users.noreply.github.com>
2025-08-12 17:44:35 -0700

    updated version

--- main.js
+++ main.js
@@ -1,5 +1,14 @@
 // add buttons that take the user to different sections
 // or hide and show said sections
 
-document.getElementById("about_us").addEventListener("click", toggleInfo)
+document.getElementById("about_us").addEventListener("click", toggleInfo);
 
+function toggleInfo() {
+    const text = document.getElementById("about_content");
+
+    if (text.style.display === "none") {
+        text.style.display = "block";
+    } else {
+        text.style.display = "none";
+    }
+}
